7c8f4173828f2efc3c1cfb57c1831b2ca6a15d47
sammcgrail <sammcgrail@gmail.com>
2021-09-14 20:15:04 -0400

    boilerplate for stopwatch

---
+++ src/stopwatch.js
@@ -0,0 +1,13 @@
+console.log("stopwatchjsfile");
+
+function startTimer() {
+  console.log("Timer started!");
+};
+
+function stopTimer() {
+  console.log("Timer stopped!");
+};
+
+function resetTimer() {
+  console.log("Timer reset!");
+};
