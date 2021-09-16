32a3a50b1a99b31d4a581690cfd62d0d50d86502
sammcgrail <sammcgrail@gmail.com>
2021-09-15 23:53:53 -0400

    piano working

--- src/stopwatch.js
+++ src/stopwatch.js
@@ -31,18 +31,17 @@ function loop() {
 
   console.log(elapsedTime);
 
+  const elapsedMilliseconds = Math.floor(elapsedTime);
   const elapsedSeconds = Math.floor(elapsedTime / 1000);
-
   const elapsedMinutes = Math.floor(elapsedSeconds / 60);
+  const elapsedHours = Math.floor(elapsedMinutes / 60 * 60);
 
-  const displaySeconds = elapsedSecondsxdl;;lkjh cxz
+  const displayMilliseconds = String(elapsedMilliseconds % 1000).padStart(3, '0');
+  const displaySeconds = String(elapsedSeconds % 60).padStart(2, '0');
+  const displayMinutes = String(elapsedMinutes % 60).padStart(2, '0');
+  const displayHours = String(elapsedHours % 24).padStart(2, '0');
 
-  const displayMinutes =
-
-
-    document.getElementById("time").innerText = elapsedMinutes + ":" + elapsedSeconds;
-
-
+  document.getElementById("time").innerText = `${displayHours}:${displayMinutes}:${displaySeconds}:${displayMilliseconds}`;
 
   lastLoopTime = performance.now();
 
