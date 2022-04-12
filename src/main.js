71157909ad5337c3078f9583f7fa4f5883d7fd11
Sam <sammcgrail@gmail.com>
2022-04-12 13:53:29 -0400

    remove unneeded packages, update resume, clean up files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var util_1 = require("./util");
+// import * as tf from '@tensorflow/tfjs';
 console.log(util_1.squared(3));
 // tf.abs(-19);
