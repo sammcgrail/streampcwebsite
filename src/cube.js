c4ac76904df5c8621de6f5b3685d82ceb14b7489
Sam McGrail <sammcgrail@gmail.com>
2023-05-05 15:48:41 -0400

    get a better tut

--- src/cube.js
+++ src/cube.js
@@ -3,3 +3,33 @@ THREE.ArcCurve;
 console.log(THREE.ArcCurve);
 
 console.log("cube javascript file opens");
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
