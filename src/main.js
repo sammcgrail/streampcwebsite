22708b8ae66482281935f135abc9616366775ba2
Sam McGrail <sammcgrail@gmail.com>
2025-02-25 01:04:25 -0500

    remove old files

--- src/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var util_1 = require("./util");
-// import * as tf from '@tensorflow/tfjs';
-console.log(util_1.squared(3));
-// tf.abs(-19);
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { squared } from "./util";
-// import * as tf from '@tensorflow/tfjs';
-
-console.log(squared(3))
-
-// tf.abs(-19);
